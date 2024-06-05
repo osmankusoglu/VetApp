66c4604b609029385cfe7707d625f9b8fc0059d6
Osman Kuşoğlu <kusogluo@gmail.com>
2024-06-05 04:04:55 +0300

    Update Customer Page

--- src/Pages/Customer/Customer.jsx
+++ src/Pages/Customer/Customer.jsx
@@ -1,5 +1,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./Customer.css";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
 
 function Customer() {
   const [customer, setCustomer] = useState();
@@ -96,116 +111,212 @@ function Customer() {
       .then(() => setUpdate(false));
   };
 
+  /////////////////////////////////
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
   return (
     <div>
-      <div>
-        <h3>Update Customer</h3>
+      <div className="div-table">
+        <div>
+          <h3>Add New Customer</h3>
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={newCustomer.name}
+            onChange={handleNewCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={newCustomer.email}
+            onChange={handleNewCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="phone"
+            placeholder="Phone"
+            value={newCustomer.phone}
+            onChange={handleNewCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="address"
+            placeholder="Address"
+            value={newCustomer.address}
+            onChange={handleNewCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="city"
+            placeholder="City"
+            value={newCustomer.city}
+            onChange={handleNewCustomerInputChange}
+          />
+          <br />
+          <br />
+          <Button
+            variant="contained"
+            color="success"
+            onClick={handleAddNewCustomer}
+          >
+            Add Customer
+          </Button>
+          <br />
+          <br />
+        </div>
         <br />
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={updateCustomer.name}
-          onChange={handleUpdateCustomerInputChange}
-        />
+        <div>
+          <h3>Update Customer</h3>
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={updateCustomer.name}
+            onChange={handleUpdateCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={updateCustomer.email}
+            onChange={handleUpdateCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="phone"
+            placeholder="Phone"
+            value={updateCustomer.phone}
+            onChange={handleUpdateCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="address"
+            placeholder="Address"
+            value={updateCustomer.address}
+            onChange={handleUpdateCustomerInputChange}
+          />
+          <br />
+          <TextField
+            variant="standard"
+            type="text"
+            name="city"
+            placeholder="City"
+            value={updateCustomer.city}
+            onChange={handleUpdateCustomerInputChange}
+          />
+          <br />
+          <br />
+          <Button
+            variant="contained"
+            color="success"
+            onClick={handleUpdateCustomer}
+          >
+            Update Customer
+          </Button>
+        </div>
         <br />
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={updateCustomer.email}
-          onChange={handleUpdateCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="phone"
-          placeholder="Phone"
-          value={updateCustomer.phone}
-          onChange={handleUpdateCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          value={updateCustomer.address}
-          onChange={handleUpdateCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="city"
-          placeholder="City"
-          value={updateCustomer.city}
-          onChange={handleUpdateCustomerInputChange}
-        />
-        <br />
-        <button onClick={handleUpdateCustomer}>Update Customer</button>
       </div>
-      <div>
-        <h3>Add New Customer</h3>
-        <br />
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={newCustomer.name}
-          onChange={handleNewCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={newCustomer.email}
-          onChange={handleNewCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="phone"
-          placeholder="Phone"
-          value={newCustomer.phone}
-          onChange={handleNewCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          value={newCustomer.address}
-          onChange={handleNewCustomerInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="city"
-          placeholder="City"
-          value={newCustomer.city}
-          onChange={handleNewCustomerInputChange}
-        />
-        <br />
-        <button onClick={handleAddNewCustomer}>Add Customer</button>
-      </div>
-      <hr />
-      <hr />
-      <hr />
-      <ul>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Customer Name</StyledTableCell>
+              <StyledTableCell align="left">Customer Email</StyledTableCell>
+              <StyledTableCell align="left">Customer Phone</StyledTableCell>
+              <StyledTableCell align="left">Customer Address</StyledTableCell>
+              <StyledTableCell align="left">Customer City</StyledTableCell>
+              <StyledTableCell align="left">Customer Update</StyledTableCell>
+              <StyledTableCell align="left">Customer Delete</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customer?.map((cust, index) => (
+              <StyledTableRow key={index}>
+                <StyledTableCell component="th" scope="row">
+                  {cust.name}
+                </StyledTableCell>
+                <StyledTableCell align="left">{cust.email}</StyledTableCell>
+                <StyledTableCell align="left">{cust.phone}</StyledTableCell>
+                <StyledTableCell align="left">{cust.address}</StyledTableCell>
+                <StyledTableCell align="left">{cust.city}</StyledTableCell>
+                <StyledTableCell align="left">
+                  <Button
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    onClick={handleUpdateCustomerBtn}
+                    id={index}
+                  >
+                    UPDATE
+                  </Button>
+                </StyledTableCell>
+                <StyledTableCell align="left">
+                  <Button
+                    variant="outlined"
+                    startIcon={<DeleteIcon />}
+                    onClick={handleDeleteCustomer}
+                    id={cust.id}
+                  >
+                    DELETE
+                  </Button>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* <ul>
         {customer?.map((cust, index) => (
           <li key={index}>
-            {cust.name} - {cust.email} - {cust.phone} - {cust.address} -{" "}
-            {cust.city} -{" "}
-            <span onClick={handleDeleteCustomer} id={cust.id}>
+            <Button onClick={handleDeleteCustomer} id={cust.id}>
               DELETE
-            </span>{" "}
+            </Button>{" "}
             -{" "}
-            <span onClick={handleUpdateCustomerBtn} id={index}>
+            <Button onClick={handleUpdateCustomerBtn} id={index}>
               UPDATE
-            </span>
+            </Button>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
